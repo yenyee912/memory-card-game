604427ba0692c886d17a42099eed2590fda532af
yenyee912 <fuyazi1997@gmail.com>
2021-12-14 21:36:35 +0800

    v1.0.1

--- setting.js
+++ setting.js
@@ -15,21 +15,68 @@ function openTab(evt, tabName) {
   $(`#${tabName}`).addClass("active");
   $(`#btn-${tabName}`).addClass("tabButtonSelected");
 
+  if (tabName== "tabs-1"){
+    displayCards(sessionStorage.getItem("cardNumber"))
+  }
+
   console.log(`#${tabName}`)
 
 }
 
-function displayCards(){
-  for (let row=0; row<6; row++){
-    for (let col=0; col<8; col++){
-      
-      
+function displayCards(cardCountToDisplay){
+
+  let cardList= []
+  let row, col
+  $("#cards").empty()
+
+  for (let i=1; i<= 24; i++){
+    cardList.push(`card${i}`)
+  }
+  if (cardCountToDisplay== 8){
+    row=8
+    col=1
+  }
+
+  if (cardCountToDisplay == 16) {
+    row = 8
+    col = 2
+  }
+
+  if (cardCountToDisplay == 24) {
+    row = 8
+    col = 3
+  }
+
+  if (cardCountToDisplay == 32) {
+    row = 8
+    col = 4
+  }
+
+  if (cardCountToDisplay == 40) {
+    row = 8
+    col = 5
+  }
+
+  if (cardCountToDisplay == 48) {
+    row = 8
+    col = 6
+  }
+
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }  
+
+  for (let x=0; x<row; x++){
+    for (let y=0; y<col; y++){
+      $("#cards").prepend(`<img src="./images/back.png" id="${getRandomArbitrary(1,24)}" />`)
     }
   }
 }
 
 $(document).ready(() => {
+  sessionStorage.clear()
   // default open
+  sessionStorage.setItem("cardNumber", 48);
   $("#btn-tabs-1").click()
 
   $("#save_settings").click(() => {
@@ -41,6 +88,8 @@ $(document).ready(() => {
     }
 
     sessionStorage.setItem("playerName", playerName);
+    sessionStorage.setItem("cardNumber", cardNumber);
+    displayCards(sessionStorage.getItem("playerName"))
     $("#player").html("Player: " + sessionStorage.getItem("playerName"))
 
     console.log(playerName, cardNumber)
